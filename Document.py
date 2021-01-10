5360e0636473017f737e9bf32270b61b9b0d866d
AineriHari <54037730+AineriHari@users.noreply.github.com>
2021-01-10 22:23:48 +0530

    Create Document.py

---
+++ Document.py
@@ -0,0 +1,56 @@
+"""
+1. Python is a fastest growing language in terms of
+    a. number of developers who are using
+    b. number of libraries
+    c. number of companies who are using
+    d. number of areas implemented
+        i. Machine Learning
+        ii. GUI
+        iii. Software developement
+        iv. Web Developement
+        so it called as General purpose language.
+        
+2. It is a Interpreted, Object-Oriented and High-level language
+    a. Interpreted: it reads the program from step by step later start to execute
+    b. Supports Object-Oriented, Procedure and functional language.
+    c. High-level: user can able to understand the language which is called as High-level language.
+    
+3. Python came into 1989 founded by Guido Van Rossum.
+    He is big fan of British movie called "Moty Python Flying Circus". So he went that name Python.
+    
+4. Python 1.0 - January 1994
+   Python 2.0 - October 2000
+   Python 3.0 - December 2008
+   
+   Python 2.0 and Python 3.0 are both different types of versions. So no backword comfortability from Python 2.0 to Python 3.0
+   
+5. Python IDE (Integrated Developement Environment)
+   IDE: >>> is a interactive shell
+   REPL:
+   R - Read
+   E - Execute
+   P - Print
+   L - Loop
+   
+6. BODMAS Rule: Brackets((), {},[]), Orders(x**2, sprt(x)), Division, Multiple, Addition and Subtraction.
+7. "\" it will skip the special meaning of the any valid sysmbols in Python.
+8. variable: variable is a container which stores the data in it.
+   How variables are assigned in Python?
+        1. create a new object which is depending on the data type (int, str, float and bool).
+        2. add the data to an object.
+        3. assign the name which is user name to object.
+9. variables are dynamic type which means we can change values.
+    variables are immutables we can change data in same address.
+    
+10. data types:
+    None, Numeric (int, float and bool), sequence (list, tuple, range, set, string) and mapping or dictionary
+    
+11. Operators:
+    1. Arthimetic
+    2. assigment
+    3. relational or comaparision
+    4. logical
+    5. bitwise
+    6. identity
+    7. membership"""
+
